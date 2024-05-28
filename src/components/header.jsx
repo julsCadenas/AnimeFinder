9a7a5b320191456661e9f8730c9d59db721a0f97
julsCadenas <jscadenas12@gmail.com>
2024-05-28 10:58:38 +0800

    fixed loading text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,16 +4,16 @@ const Header = () => {
     
     useEffect(() => {
         const handleMouseMove = (e) => {
-            const header = document.querySelector("header");
-            const rect = header.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
-            header.style.setProperty("--mouse-x", `${x}px`);
-            header.style.setProperty("--mouse-y", `${y}px`);
+            const header = document.querySelector("header")
+            const rect = header.getBoundingClientRect()
+            const x = e.clientX - rect.left
+            const y = e.clientY - rect.top
+            header.style.setProperty("--mouse-x", `${x}px`)
+            header.style.setProperty("--mouse-y", `${y}px`)
         };
 
-        const header = document.querySelector("header");
-        header.addEventListener("mousemove", handleMouseMove);
+        const header = document.querySelector("header")
+        header.addEventListener("mousemove", handleMouseMove)
 
         return () => {
             header.removeEventListener("mousemove", handleMouseMove);
